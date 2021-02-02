167ec415b405988c1c959e67638356fc759f3afc
JaoK <jjaokk@gmail.com>
2021-02-02 15:42:45 +0100

    income gets added to the list

--- html_version/src/main/webapp/script.js
+++ html_version/src/main/webapp/script.js
@@ -1,6 +1,10 @@
 function addIncome(){
     var income = prompt("Add your income");
-    alert (income);
+    var div = document.getElementById("income");
+    
+    div.innerHTML += "<hr/><div class='row'><div class='col-6'>Paycheck 1</div><div class='col-3'>"+income+" kr</div><div class='col-3'> 0 kr</div> </div>";
+    
+    
 }
 
 function addExpense(){
