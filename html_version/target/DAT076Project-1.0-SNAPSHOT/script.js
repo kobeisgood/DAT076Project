571b2901d665145108247abb3ad16cb274993bc4
Marcus Axelsson <marcusaxelsson52@gmail.com>
2021-02-02 11:03:47 +0100

    Fixed issues

---
+++ html_version/target/DAT076Project-1.0-SNAPSHOT/script.js
@@ -0,0 +1,12 @@
+function addIncome(){
+    var income = prompt("Add your income");
+    alert (income);
+}
+
+function addExpense(){
+    var expense = prompt("Add your expense");
+    alert (expense);
+}
+
+
+
