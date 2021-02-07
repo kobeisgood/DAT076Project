17aae40b185c1d81a01596badb77ced219a2e4b5
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2021-02-07 15:53:08 +0100

    refactored the dashboard card
    Co-authored-by: Daniel Karlkvist <danielkarlkvist@gmail.com>

--- react_version/src/pages/Dashboard.js
+++ react_version/src/pages/Dashboard.js
@@ -9,16 +9,16 @@ export default class Dashboard extends React.Component{
         <div class="container-fluid">
         <div class="d-flex ">
         <div class="col-sm">
-          <DashboardCard/>
+          <DashboardCard month="Januari" year="2021" income="1" expense="2" savings="3"/>
         </div>
         <div class="col-sm">
-          <DashboardCard/>
+          <DashboardCard month="February" year="2021" income="4" expense="5" savings="6"/>
         </div>
         <div class="col-sm">
-          <DashboardCard/>
+          <DashboardCard month="March" year="2021" income="7" expense="8" savings="9"/>
         </div>
         <div class="col-sm">
-          <DashboardCard/>
+          <DashboardCard month="April" year="2021" income="10" expense="11" savings="12"/>
         </div>
         </div>
     </div>
