f598a73c96313758e8e44c9a78579b3cd4feaa41
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-07 11:51:42 +0100

    Added inputs and background to add income popup

--- frontend-react/src/main/webapp/src/components/AddIncomePopup.js
+++ frontend-react/src/main/webapp/src/components/AddIncomePopup.js
@@ -7,9 +7,14 @@ export default class AddIncomePopup extends React.Component {
         return(
             <div className="full-page-container">
                 <div className="flexbox-container">
-                    <p>hej</p>
-                    <p>hej</p>
-                    <p>hej</p>
+                    <div className="add-income-popup">
+                        <h3>Add income</h3>
+                        <div className="add-income-input-container">
+                            <input className="add-income-name-input" placeholder="Name (e.g: salary)"></input>
+                            <br></br>
+                            <input className="add-income-amount-input" placeholder="Amount"></input>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
