6ccff4a4c04d1e097cd91e95c5c1e11ec67a4dcd
Theodor Khademi <theo.khademi@gmail.com>
2021-03-07 14:10:37 +0100

    Added states for edit and some functionality, fixed styling
    Co-authored-by:  Marcus Axelsson <marcusaxelsson52@gmail.com>

--- frontend-react/src/main/webapp/src/pages/Profile.js
+++ frontend-react/src/main/webapp/src/pages/Profile.js
@@ -2,27 +2,41 @@ import React from 'react';
 import "../css/profile.css";
 
 export default class Profile extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          editState: false
+        };
+      }
+
+      setEditState() {
+          this.setState({editState: !this.state.editState})
+          console.log("edit state is now " + !this.state.editState)
+      }
+
     render() {
+
         return(
             <div className="profile-page-container">
                 <div className="profile-card-container">
                     <div className="profile-pic"></div>
                     <div className="profile-info">
                         <div className="info-row">
-                            <span>Name:</span>
-                            <span>Marcus</span>
+                            <span className="left-info-item"> Name:</span>
+                            <span className="right-info-item"> Marcus</span> {/*should be taken from DB*/}
                         </div>
                         <div className="info-row">
-                            <span>Last name:</span>
-                            <span>Axelsson</span>
+                            <span className="left-info-item">Last name:</span>
+                            <span className="right-info-item">Axelsson</span> {/*should be taken from DB*/}
                         </div>
                         <div className="info-row">
-                            <span>Email:</span>
-                            <span>marcusaxelsson52@gmail.com</span>
+                            <span className="left-info-item">Email:</span>
+                            <span className="right-info-item">marcusaxelsson52@gmail.com</span> {/*should be taken from DB*/}
                         </div>
                     </div>
-                    <div className="edit-container">
-                        <p className="edit-button-text">Edit profile</p>
+                    <div className="edit-container" onClick={() => this.setEditState()}>
+                        <p>Edit profile</p>
                     </div>
                 </div>
             </div> 
