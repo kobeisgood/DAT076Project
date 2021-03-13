18c24eb00be04632f1afa93c62362743c89a0b82
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-13 14:37:48 +0100

    Changed structure of dashboard card and some UI

--- frontend-react/src/main/webapp/src/components/DashboardCard.js
+++ frontend-react/src/main/webapp/src/components/DashboardCard.js
@@ -1,96 +1,87 @@
 import React from 'react';
 import Chart from 'chart.js';
-import '../css/dashboard.css';
 import '../main.css';
+import '../css/dashboard.css';
 
 import { Link } from 'react-router-dom';
 
 
 export default class DashBoardCard extends React.Component {
 
-  
 
-constructor(props){
-  super();
-  this.props = props;
-  if(!this.props.data.summary.INCOME)
-    this.props.data.summary.INCOME = Math.floor(Math.random() * 2500);  ///////////////////// TESTING! Should be 0!
-  if(!this.props.data.summary.EXPENSE)
-    this.props.data.summary.EXPENSE = Math.floor(Math.random() * 2500); ///////////////////// TESTING! Should be 0!
-  if(!this.props.data.summary.SAVINGS)
-    this.props.data.summary.SAVINGS = Math.floor(Math.random() * 2500); ///////////////////// TESTING! Should be 0!
-}
-  
-chartRef2 = React.createRef();
 
-componentDidMount() {
-  const doughnutChartRef = this.chartRef2.current.getContext("2d");
+	constructor(props) {
+		super();
+		this.props = props;
+		if (!this.props.data.summary.INCOME)
+			this.props.data.summary.INCOME = Math.floor(Math.random() * 2500);  ///////////////////// TESTING! Should be 0!
+		if (!this.props.data.summary.EXPENSE)
+			this.props.data.summary.EXPENSE = Math.floor(Math.random() * 2500); ///////////////////// TESTING! Should be 0!
+		if (!this.props.data.summary.SAVINGS)
+			this.props.data.summary.SAVINGS = Math.floor(Math.random() * 2500); ///////////////////// TESTING! Should be 0!
+	}
 
- 
-   
-        new Chart(doughnutChartRef, {
-        type:"doughnut",
-        data: {
-          //Bring in data
-          labels: ["INCOME","EXPENSE","SAVINGS"],
-          datasets: [
-              {
-                  label: ["Utgifter <månad, år>"],
-                  backgroundColor:["#5cff80","#ff7777","#8f85ff"],
-                  data: [this.props.data.summary.INCOME,this.props.data.summary.EXPENSE,this.props.data.summary.SAVINGS],
-              }
-          ]
-        },
+	chartRef2 = React.createRef();
 
-        options: {
-          title: {
-            display: false,
-            text: 'Utgifter <månad, år> procentuellt'
-          },
-          legend: {
-            display: false
-          }
-        },
-
-        });
-
-}
-
-sum(){
-  return this.props.data.summary.INCOME-this.props.data.summary.EXPENSE+this.props.data.summary.SAVINGS;
-}
-
-monthToName(month){
-
-  var months = ["Januari","Februari","Mars","April","Maj","Juni","Juli","Augusti","September","Oktober","November","December"];
-  return months[month-1];
-
-}
+	componentDidMount() {
+		const doughnutChartRef = this.chartRef2.current.getContext("2d");
 
 
 
-  render() {
+		new Chart(doughnutChartRef, {
+			type: "doughnut",
+			data: {
+				//Bring in data
+				labels: ["INCOME", "EXPENSE", "SAVINGS"],
+				datasets: [
+					{
+						label: ["Utgifter <månad, år>"],
+						backgroundColor: ["#5cff80", "#ff7777", "#8f85ff"],
+						data: [this.props.data.summary.INCOME, this.props.data.summary.EXPENSE, this.props.data.summary.SAVINGS],
+					}
+				]
+			},
 
-    return(
-      <div class="dashboard-card box-shadow">
+			options: {
+				title: {
+					display: false,
+					text: 'Utgifter <månad, år> procentuellt'
+				},
+				legend: {
+					display: false
+				}
+			},
 
-        <p class="dashboard-card-name">{this.monthToName(this.props.data.month)} {this.props.data.year}</p>
+		});
 
-        <canvas className="chartStyling" id="doughnutChart" ref={this.chartRef2}></canvas>
+	}
 
-        <div class="row">
-            <div class="col">
-            <p class={this.sum() > 0 ? "color-money-left" : "color-no-money-left"}>{this.sum()}</p>
-            </div>
-        </div>
-        <div className="buttonStyling">
-          <Link className="cardButtonText cardButton" to="/frontend-react/monthly">
-            Go to view
-          </Link>
-        </div>
-      </div>
-    );
-  }
+	sum() {
+		return this.props.data.summary.INCOME - this.props.data.summary.EXPENSE + this.props.data.summary.SAVINGS;
+	}
+
+	monthToName(month) {
+
+		var months = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+		return months[month - 1];
+
+	}
+
+
+
+	render() {
+
+		return (
+			<div className="dashboard-card-container box-shadow">
+				<div className="dashboard-card-flexbox">
+					<p class="dashboard-card-name">{this.monthToName(this.props.data.month)} {this.props.data.year}</p>
+					<canvas className="chartStyling" id="doughnutChart" ref={this.chartRef2}></canvas>
+					<p class={this.sum() > 0 ? "color-money-left sum-text" : "color-no-money-left sum-text"}>{this.sum()}</p>
+					<Link className="dashboard-card-button hover-transition" to="/frontend-react/monthly">Go to view</Link>
+				</div>
+			</div>
+		);
+	}
 
 
 
