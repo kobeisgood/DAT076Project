a4a713ef6ea35ea9cd1a70405cf444e155c2e362
JaoK <jjaokk@gmail.com>
2021-03-13 19:53:22 +0100

    Update Sidebar.js
    Co-Authored-By: Theodor Khademi <theo.khademi@gmail.com>
    Co-Authored-By: Daniel Karlkvist <danielkarlkvist@gmail.com>
    Co-Authored-By: Robin Repo Wecklauf <wecklauf@student.chalmers.se>
    Co-Authored-By: Marcus Axelsson <33228116+Macishh@users.noreply.github.com>

--- frontend-react/src/main/webapp/src/components/Sidebar.js
+++ frontend-react/src/main/webapp/src/components/Sidebar.js
@@ -1,11 +1,86 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-
-
-import { SidebarData } from './SidebarData';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSignOutAlt, faTachometerAlt} from '@fortawesome/free-solid-svg-icons';
+import {faCalendarAlt} from '@fortawesome/free-solid-svg-icons';
+import {faChartBar} from '@fortawesome/free-solid-svg-icons';
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
 import '../css/Sidebar.css';
 
-function Sidebar() {
+
+export default class Sidebar extends React.Component {
+
+ state = {SidebarData:[]}
+ 
+  
+
+async componentDidMount() {
+
+    const url = "http://localhost:8080/frontend-react/api/users/session";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    var SidebarData;
+    if(data){ // logged in
+        SidebarData = [
+            {
+                title: "Dashboard",
+                path: "/frontend-react/dashboard",
+                icon: <FontAwesomeIcon icon={faTachometerAlt} color='white' size='lg'/>,
+                cName: "nav-text"
+            },
+            {
+                title: "Monthly",
+                path: "/frontend-react/monthly",
+                icon: <FontAwesomeIcon icon={faCalendarAlt} color='white' size='lg'/>,
+                cName: "nav-text"
+            },
+            {
+                title: "Graph",
+                path: "/frontend-react/graph",
+                icon: <FontAwesomeIcon icon={faChartBar} color='white' size='lg'/>,
+                cName: "nav-text"
+            },
+            {
+                title: "Profile",
+                path: "/frontend-react/profile",
+                icon: <FontAwesomeIcon icon={faUserCircle} color='white' size='lg'/>,
+                cName: "nav-text"
+            },
+            {
+                title: "Sign out",
+                path: "/frontend-react/logout",
+                icon: <FontAwesomeIcon icon={faSignOutAlt} color='white' size='lg'/>,
+                cName: "nav-text"
+            }
+        ];
+    }
+    else{
+        SidebarData = [
+            
+            {
+                title: "Start",
+                path: "/frontend-react/start",
+                icon: <FontAwesomeIcon icon={faHome} color='white' size='lg'/>,
+                cName: "nav-text"
+            }
+        ];
+    }
+
+    
+    console.log(SidebarData);
+    this.setState({SidebarData});
+
+    }
+
+
+ render() {
+
+
+
+
+
     return(
         /*
         <nav className={"nav-menu"}>
@@ -17,7 +92,7 @@ function Sidebar() {
             <h1 className='logo'> CashIT </h1>
             <ul className="nav-menu-items">
                  
-                {SidebarData.map((item, index) => {
+                {this.state.SidebarData.map((item, index) => {
                     return (
                         <li key={index} className={item.cName}>
                             <Link to={item.path}>
@@ -35,4 +110,4 @@ function Sidebar() {
     )
 }
 
-export default Sidebar; 
+}
