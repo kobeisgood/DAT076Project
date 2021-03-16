f9092d04d4f6673ef53f97cc8fd6abd650bfdd86
Daniel Karlkvist <danielkarlkvist@gmail.com>
2021-03-16 14:10:49 +0100

    Fixed styling on monthly floating buttons
    Co-authored-by: Joakim Ohlsson <jjaokk@gmail.com>
    Co-authored-by: Theodor Khademi <theo.khademi@gmail.com>
    Co-authored-by: Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
    Co-authored-by: Marcus Axelsson <marcusaxelsson52@gmail.com>

--- frontend-react/src/main/webapp/src/components/MonthlyFloatingButtons.js
+++ frontend-react/src/main/webapp/src/components/MonthlyFloatingButtons.js
@@ -5,37 +5,38 @@ export default class MonthlyFloatingButtons extends React.Component {
 
     openAddCategoryPopup() {
         var element = document.getElementById("add-category-popup");
-         // element.setAttribute("category",this.state.category);
-          element.style.visibility = "visible";
+        // element.setAttribute("category",this.state.category);
+        element.style.visibility = "visible";
 
-         // Hide floating buttons
-         document.getElementById("newCategoryFloat").style.visibility = "hidden"
-         document.getElementById("newTransactionFloat").style.visibility = "hidden"
-        
-      }
-
-      openAddTransactionPopup() {
-        var element = document.getElementById("add-transaction-popup");
-         // element.setAttribute("category",this.state.category);
-          element.style.visibility = "visible";
-
-          // Hide floating buttons
+        // Hide floating buttons
         document.getElementById("newCategoryFloat").style.visibility = "hidden"
         document.getElementById("newTransactionFloat").style.visibility = "hidden"
-      }
+
+    }
+
+    openAddTransactionPopup() {
+        var element = document.getElementById("add-transaction-popup");
+        // element.setAttribute("category",this.state.category);
+        element.style.visibility = "visible";
+
+        // Hide floating buttons
+        document.getElementById("newCategoryFloat").style.visibility = "hidden"
+        document.getElementById("newTransactionFloat").style.visibility = "hidden"
+    }
 
     render() {
-        return(
+        return (
             <div>
-                <div id="newTransactionFloat" className="float2" onClick={this.openAddTransactionPopup}>
-                Add New Transaction
-            </div>
-            <div id="newCategoryFloat" className="float1" onClick={this.openAddCategoryPopup}>
-                Add New Category
-            </div>
+                <button id="newTransactionFloat" className="add-transaction-popup-button add-popup-button" onClick={this.openAddTransactionPopup}>
+                    Add New Transaction
+                </button>
+
+                <button id="newCategoryFloat" className="add-category-popup-button add-popup-button" onClick={this.openAddCategoryPopup}>
+                    Add New Category
+                </button>
 
             </div>
-            
+
         )
     }
 
